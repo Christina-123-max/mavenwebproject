3c4da0e7174fa9f43d6dac1d518e6bc0593c3cf8
christy-22bd1a664d <yerrachristina@gmail.com>
2024-11-22 10:43:18 +0530

    maven web project

---
+++ src/main/java/com/app/demo/mavenjavapro/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.mavenjavapro;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("name:christina,roll no 22bd1a664d");
+    }
+}
